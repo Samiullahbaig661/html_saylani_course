e4d7ca5c71b6bb8750252b8548357c3018a99790
Samiullahbaig661 <samiullahbaig661@gmail.com>
2024-12-26 19:54:10 +0500

    yeh mere lab 13 ki file hai lakin yeh usefull hai is mai kuch component pary hue hai

--- javaScript codes/average.js
+++ javaScript codes/average.js
@@ -1,15 +1,29 @@
-var a  = parseFloat(prompt("Enter the matric percentage"))
-var b = parseFloat(prompt("Enter the percentage of saylani mass it traning programme"))
-if (a > 100 || b > 100 ) {
-    alert("Enter the valid percentage")
-}
-else {
-    var c = (a + b)
-    var avg =c/2 
-    if (avg >70 ) {
-        alert("You are eligible")
-    }
-    else{
-        alert("You are not eligible")
-    }
-}   
+// var a  = parseFloat(prompt("Enter the matric percentage"))
+// var b = parseFloat(prompt("Enter the percentage of saylani mass it traning programme"))
+// if (a > 100 || b > 100 ) {
+//     alert("Enter the valid percentage")
+// }
+// else {
+//     var c = (a + b)
+//     var avg =c/2 
+//     if (avg >70 ) {
+//         alert("You are eligible")
+//     }
+//     else{
+//         alert("You are not eligible")
+//     }
+// }   
+
+
+
+
+
+
+// let a = prompt("Enter name")
+// let b = a.length 
+// for (let i = 0; i < b; i++) {
+//    if (a.slice(i,i+2) == " ") {
+//     alert("no two spaces consecutive")
+    
+//    }
+// }
